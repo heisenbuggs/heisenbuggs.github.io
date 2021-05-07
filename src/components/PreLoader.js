afb977978148a2507668273edea98a8411639dfc
coderprasukj <prasukjain0047@gmail.com>
2021-05-07 20:54:38 +0530

    PreLoader

--- src/components/PreLoader.js
+++ src/components/PreLoader.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/PreLoader.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Pre = (props) => {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
-}
+  const [loader, setLoader] = useState(props.load);
+  const close = () => {
+    setTimeout(() => {
+      setLoader(false)
+    }, 2000)
+  };
+  return (
+    <div id={loader ? "preloader" : "preloader-none"}>
+      {(props.load || loader) && close()}
+    </div>
+  )
+};
 
 export default Pre;
